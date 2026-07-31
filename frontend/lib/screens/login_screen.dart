44cf7e3ba0b5c4f94d3f48272b007c11eac0c7b2
Sai-shashank-2005 <saishashankpedapati@gmail.com>
2026-07-31 09:29:33 +0530

    update flutter ci workflow

--- frontend/lib/screens/login_screen.dart
+++ frontend/lib/screens/login_screen.dart
@@ -39,8 +39,6 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
